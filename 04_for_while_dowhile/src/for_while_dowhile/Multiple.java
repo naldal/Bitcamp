e71d74a1d8c3cb3503eecd84bc9ae55be58efdca
naldal <cec7378@gmail.com>
2019-12-20 14:19:36 +0900

    d

---
+++ 04_for_while_dowhile/src/for_while_dowhile/Multiple.java
@@ -0,0 +1,5 @@
+package for_while_dowhile;
+
+public class Multiple {
+
+}
--- 04_for_while_dowhile/src/for_while_dowhile/NumberGame.java
+++ 04_for_while_dowhile/src/for_while_dowhile/NumberGame.java
@@ -10,10 +10,25 @@ public class NumberGame {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int usrNum;
+		String veri;
 		int computRan = (int)(Math.random()*100)+1;
 		int cnt=0;
 		
-		while(true) {
+//		while(true) {
+//			System.out.print("숫자입력 : ");
+//			usrNum = Integer.parseInt(br.readLine());
+//			cnt++;
+//			if(usrNum > computRan) {
+//				System.out.println(usrNum+"보다 작습니다.");
+//			} else if (usrNum < computRan) {
+//				System.out.println(usrNum+"보다 큽니다.");
+//			} else {
+//				System.out.println("딩동댕 "+cnt+"번만에  맞추셨습니다.");
+//				break;
+//			}
+//		}
+		
+		do {
 			System.out.print("숫자입력 : ");
 			usrNum = Integer.parseInt(br.readLine());
 			cnt++;
@@ -22,9 +37,16 @@ public class NumberGame {
 			} else if (usrNum < computRan) {
 				System.out.println(usrNum+"보다 큽니다.");
 			} else {
-				System.out.println("딩동댕"+cnt+"만에 맞추셨습니다.");
-				break;
+				System.out.println("딩동댕 "+cnt+"번만에  맞추셨습니다.");
+				
+				System.out.println("또 할래? (Y/N)");
+				veri = br.readLine();
+				if(veri.equals("Y")) {
+					
+				} else {
+					break;
+				}
 			}
-		}
+		}while(true);
 	}
 }
